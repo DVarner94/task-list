e18079427d7ed8e0cff093cf8f3b2b50ccb735a5
Dominic Varner <dommy@MLBs-Lappy.local>
2020-09-30 14:02:35 -0700

    clearing local storage after deleting items

--- app.js
+++ app.js
@@ -108,8 +108,28 @@ function removeTask(e){
     if(e.target.parentElement.classList.contains('delete-item')){
         if(confirm('Are you sure you want to delete item?')){
             e.target.parentElement.parentElement.remove();
+
+            removeTaskFromLocalStorage(e.target.parentElement.parentElement)
         }
     }
+}
+
+// Remove from local storage
+function removeTaskFromLocalStorage(taskItem){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function(task, index){
+        if(taskItem.textContent === task){
+            tasks.splice(index, 1);
+        }
+    });
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // clear tasks
@@ -121,6 +141,14 @@ function clearTasks(){
     while(taskList.firstChild){
         taskList.removeChild(taskList.firstChild)
     }
+
+    // Clear from local storage
+    clearFromLocalStorage();
+}
+
+// Clear tasks from local storage
+function clearFromLocalStorage(){
+    localStorage.clear();
 }
 
 // filter tasks
