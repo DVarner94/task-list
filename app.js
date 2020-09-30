8e2302820524cb2495251c6e83a435c3f7293963
Dominic Varner <dommy@MLBs-Lappy.local>
2020-09-30 13:27:59 -0700

    store task items, and show on refresh

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ loadEventListeners();
 
 // Load all event listeners
 function loadEventListeners() {
+    // DOM Load event
+    document.addEventListener('DOMContentLoaded', getTasks);
     // add task event
     form.addEventListener('submit', addTask);
     // remove task event
@@ -19,6 +21,37 @@ function loadEventListeners() {
     // filter task event
     filter.addEventListener('keyup', filterTasks);
 }
+
+// Get tasks from local storage
+function getTasks(){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function(task){
+        // create li element
+        const li = document.createElement('li');
+        // add item
+        li.className = 'collection-item';
+        // Create text node and append to li
+        li.appendChild(document.createTextNode(task));
+        // Create new link element
+        const link = document.createElement('a');
+        // Add class
+        link.className = 'delete-item secondary-content';
+        // add icon
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        // append link to the li
+        li.appendChild(link);
+
+        // append li to ul
+        taskList.appendChild(li)
+    });
+}
+
 
 // add task
 function addTask(e){
@@ -45,11 +78,30 @@ function addTask(e){
     // append li to ul
     taskList.appendChild(li)
 
+    // store in local storage
+    storeTaskInLocalStorage(taskInput.value);
+
     // clear input
     taskInput.value = '';
 
     e.preventDefault();
 }
+
+// store task
+function storeTaskInLocalStorage(task){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.push(task)
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+
 
 // remove task
 function removeTask(e){
